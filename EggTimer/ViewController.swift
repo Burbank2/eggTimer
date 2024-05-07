b1fb4bd415ff0d3e3ce662c1c07b377547d9645b
Michael Burbank <burbank2@yahoo.com>
2024-05-07 15:32:57 -0500

    Update eggTimes values.

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -1,13 +1,13 @@
 import UIKit
-import AVFoundation
+//import AVFoundation
 
 class ViewController: UIViewController {
     
     @IBOutlet weak var progressBar: UIProgressView!
     @IBOutlet weak var titleLabel: UILabel!
-    let eggTimes = ["Soft": 3, "Medium": 4, "Hard": 7]
+    let eggTimes = ["Soft": 300, "Medium": 420, "Hard": 720]
     var timer = Timer()
-    var player: AVAudioPlayer!
+//    var player: AVAudioPlayer!
     var totalTime = 0
     var secondsPassed = 0
     
@@ -33,9 +33,9 @@ class ViewController: UIViewController {
             timer.invalidate()
             titleLabel.text = "DONE!"
             
-            let url = Bundle.main.url(forResource: "alarm_sound", withExtension: "mp3")
-            player = try! AVAudioPlayer(contentsOf: url!)
-            player.play()
+//            let url = Bundle.main.url(forResource: "alarm_sound", withExtension: "mp3")
+//            player = try! AVAudioPlayer(contentsOf: url!)
+//            player.play()
         }
     }
     
